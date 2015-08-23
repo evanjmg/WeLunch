e4e2d4da21291ed4f847feef288823299471dbf0
ftabor <ftaborconsulting@gmail.com>
2015-08-23 23:45:22 +0100

    Linkedin in oath - cleaning up the code

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ var passport       = require('passport');
 var layouts        = require('express-ejs-layouts');
 var sassMiddleware = require('node-sass-middleware');
 var morgan         = require('morgan');
-var ejs            = require('ejs')
+var ejs            = require('ejs');
+var flash 				 = require('connect-flash');
+
+// how do we do flashes in ajax?
 
 var cookieParser   = require('cookie-parser');
 var session        = require('express-session');
 var MongoStore     = require('connect-mongo')(session);
-// var flash          = require('connect-flash'); how do we do flashes in ajax?
+
 
 // MODELS 
 var User  = require('./models/user');
 var Event = require('./models/event');
 
 var databaseURL = process.env.MONGOLAB_URI ||'mongodb://localhost/welunch';
-    mongoose.connect(databaseURL);
+mongoose.connect(databaseURL);
 
 
 //  VIEWS
@@ -29,42 +32,43 @@ app.set('views', __dirname + '/views')
 app.engine('ejs', ejs.renderFile);
 app.set('view engine', 'ejs');
 
-
-
-
-
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 
+
 // SESSIONS
- require('./config/passport')(passport);
+require('./config/passport')(passport);
 app.use(session({
-  secret:'secret',
-  maxAge: new Date(Date.now() + 3600000),
-  store: new MongoStore({mongooseConnection:mongoose.connection})
+	secret:'secret',
+	maxAge: new Date(Date.now() + 3600000),
+	store: new MongoStore({mongooseConnection:mongoose.connection})
 }));
+app.use(cookieParser());
 
 
 // AUTHENTICATION
-
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(flash());
+app.use(flash());
+
 
 // SASS Middleware
 var srcPath = './public/sass';
 var destPath = './public/styles';
 
 app.use('/', sassMiddleware({
-  src: srcPath,
-  dest: destPath,
-  debug: true,
-  outputStyle: 'expanded'
+	src: srcPath,
+	dest: destPath,
+	debug: true,
+	outputStyle: 'expanded'
 }));
+
 
 // CONTROLLERS
 app.use(require('./controllers'));
+
+
 // PORT
 app.listen(process.env.PORT || 8000, function () {
-  console.log('listening on port 8000 - WeLunch')
+	console.log('listening on port 8000 - WeLunch')
 });
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
- 
+
 var userSchema = new mongoose.Schema({
   local: {
     name: String,
@@ -8,15 +8,38 @@ var userSchema = new mongoose.Schema({
     password: String
   },
   linkedin: {
-    _id: String,
-    name:String,
+    id: String,
+    name: String, 
+    location: String,
+    industry: String,
     avatar: String,
     access_token: String,
-},
+  },
 })
 
 var User = mongoose.model('User', userSchema);
-module.exports = User;
 
+
+//********** Hash the Password for Security.
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
+module.exports = User;
 
 
