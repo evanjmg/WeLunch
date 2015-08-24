e1b537d256113c1eea582c9ae995c48aa07dcf49
ftabor <ftaborconsulting@gmail.com>
2015-08-24 18:24:41 +0100

    deleted white space

--- public/js/ajax/login.js
+++ public/js/ajax/login.js
@@ -5,19 +5,7 @@ var toggleForm = function() {
 	$("#link-to-sign-up").on('click', function () {
 		event.preventDefault();
 		$('#local-login').fadeOut('slow');
-		$('#login-container').html("
-			<div id='local-sign-up'>
-			<strong>Sign up:</strong>
-			<a id='link-to-log-in'>Login</a>
-			<form>
-			<input type='text' id='name' name='name' value='' />
-			<input type='text' id='email' name='email' value='' />
-			<input type='text' id='password' name='password' value='' />
-			<input type='text' id='headline' name='headline' value='' />
-			<input id='callAjax' type='button' value='Call Ajax' />
-			</form>
-			<div>
-			");
+		$('#login-container').html("<div id='local-sign-up'><strong>Sign up:</strong><a id='link-to-log-in'>Login</a><form><input type='text' id='name' name='name' value='' /><input type='text' id='email' name='email' value='' /><input type='text' id='password' name='password' value='' /><input type='text' id='headline' name='headline' value='' /><input id='callAjax' type='button' value='Call Ajax' /></form><div>");
 	}); 
 
 	$('#link-to-log-in').on('click', function () {
@@ -27,29 +15,7 @@ var toggleForm = function() {
 }
 
 var generateLogIn = function () {
-	$('#login-container').html("
-		<div id='local-login'>
-		<h2>Login</h2>
-
-		<a href='/api/users/auth/linkedin'>Linkedin</a>
-		<a href='#' id='link-to-sign-up'>No Account? Sign up here</a>
-
-		<form method='post' action='/api/users/login'>
-		<div class='form-group'>
-
-
-		<div class='form-group'>
-		<label for='email'>Email</label>
-		<input class='form-control' type='text' name='email' id='email'>
-		</div>
-
-		<div class='form-group'>
-		<label for='password'>Password</label>
-		<input class='form-control' type='password' name='password' id='password'>
-		</div>
-		
-		<input class='btn btn-default' type='submit'>
-		</form></div>");
+	$('#login-container').html("<div id='local-login'><h2>Login</h2><a href='/api/users/auth/linkedin'>Linkedin</a><a href='#' id='link-to-sign-up'>No Account? Sign up here</a><form method='post' action='/api/users/login'><div class='form-group'><div class='form-group'><label for='email'>Email</label><input class='form-control' type='text' name='email' id='email'></div><div class='form-group'><label for='password'>Password</label><input class='form-control' type='password' name='password' id='password'></div><input class='btn btn-default' type='submit'></form></div>");
 }
 
 //toggle between local sign up - fade in fade out. 
