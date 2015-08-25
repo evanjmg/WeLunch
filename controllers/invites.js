e1635f6a7c654bd3bfc1afbe053b3469ef195d0f
evanjmg <evanjmg@gmail.com>
2015-08-26 00:20:54 +0100

    most of the api is working

--- controllers/invites.js
+++ controllers/invites.js
@@ -26,47 +26,60 @@ function invitesIndex(req, res) {
 function invitesCreate (req, res) {
   Event.findOne({ _owner: req.user.id }, {}, { sort: { created_at: -1} }, function (err, event) {
 
-    if (err) res.status(403).send( { message: "Could not invite user. An error occurred"})
+    if (err) res.json( { message: "Could not invite user. An error occurred"})
     // go through the event to see if the user was already invited.
-  event.invites.push({ _invitee: req.body.id });
-  event.save();
+    if(event) {
+    var i=0; for(i;i < event.invites.length;i++) {
+      if (event.invites[i]._invitee == req.user.id) {
+        res.json( { message: "You already invited this user to an event"});
+      } else {
+        event.invites.push({ _invitee: req.body.id });
+        event.save();
+        res.json(event);
+      }
+    }
+  }
+  else {
+    res.json( { message: "There is no current event"})
+  }
 });
 }
 
-function invitesApprove (req, res) {
+function invitesAccept (req, res) {
   Event.findById(req.body.event_id, function (err, event) {
-    if (err) res.status(403).send({ message: "An error occurred. Please check your request"})
+    if (err) res.json({ message: "An error occurred. Please check your request"})
       var i=0;
     for (i;i < event.invites.length;i++) {
-     if (event.invites[i]._invitee == req.body.user_id) {
-
+     if (event.invites[i]._invitee == req.user.id) {
       event.invites[i].accepted = true;
+      event.invites[i].save();
       event.save();
-      res.status(203).send( { message: "Invite accepted for event!"})
-    }
+      res.json( { message: "Invite accepted for event!"})
+    } 
   }
+  res.json({ message: "You aren't invited to any events"})
 });
 }
 
 function invitesDelete (req,res) {
   Event.findById(req.body.event_id, function (err, event) { 
-    for (i;i < event.invites.length;i++) { 
-      if (event.invites[i]._invitee == req.body.user_id) {
-        event.invites[i].accepted = false;
-
+    var i=0; for (i;i < event.invites.length;i++) { 
+      if (event.invites[i]._invitee == req.user.id) {
+        event.invites[i].remove();
+        event.invites.save();
         event.save();
-        res.status(203).send({ message: "Invite deleted"})
+        res.json({ message: "Invite deleted"})
       }
 
     }  
-    res.status(400).send({ message: "Could not find user"});
+    res.json({ message: "Could not find user"});
   }
   );
 }
 
 module.exports = {
   invitesIndex: invitesIndex,
-  invitesApprove: invitesApprove,
+  invitesAccept: invitesAccept,
   invitesCreate: invitesCreate,
   invitesDelete: invitesDelete
 }
