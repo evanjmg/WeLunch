b7389c84f86a1fb15638c63d6a6755c98f6d1259
evanjmg <evanjmg@gmail.com>
2015-08-25 20:07:25 +0100

    changed routes

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,12 @@
-module.exports = function(req, res) {
-	
-	res.render("home");
-};
+function login(req, res){
+  res.render('users/login');
+}
+
+function home(req, res){
+  res.render('index');
+}
+
+module.exports = {
+  login: login,
+  home: home
+}
