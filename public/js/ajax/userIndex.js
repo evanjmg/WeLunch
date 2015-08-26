9146075288b060e1fade3577d5919f28d5c36fc2
ftabor <ftaborconsulting@gmail.com>
2015-08-27 00:51:28 +0100

    User Index layout. Show Event layout. Presentation draft

--- public/js/ajax/userIndex.js
+++ public/js/ajax/userIndex.js
@@ -15,12 +15,12 @@ function getUsers (){
 		//Loop through the users and create an HTML string
 		var html='',i=0;
 		for(i;i< data.users.length;i++) {
-			html += "<div class='row'><div class='large-12 columns'><div class='row'><img src='"+ data.users[i].linkedin.avatar+ "'><button href='#' class='close inviteButton'><div class='alert-box'><input type='hidden' value='"+ data.users[i]._id+"' class='invitee-id'><strong>Invite</strong>'"+ data.users[i].local.name+ "'+</button></div></div></div></div>"
+			html += "<div class='row'><div class='large-12 columns'><div class='row'><div class='large-4 small-12 columns'><h4>Invite People</h4></div><div class='row'><div class='large-12 columns'><div class='panel'><div class='row'><div class='large-2 small-6 columns'><img src='"+ data.users[i].linkedin.avatar+ "'></div><div class='large-10 small-6 columns'><div class='alert-box'><button href='#' class='close inviteButton'><input type='hidden' value='"+ data.users[i]._id+"' class='invitee-id'><strong>Invite</strong> "+ data.users[i].local.name+ "</button></div></div></div></div></div></div></div></div></div>"
 		}
 
 		$('#usersInvite').append(html)
 
-	});
+	}); 
 }
 function postInvite (userid) {
 	$.post("/api/invite", { "userId" : userid }).done(function (data) {
