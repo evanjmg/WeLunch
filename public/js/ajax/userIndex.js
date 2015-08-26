f81d8e675957425500eaaa86bda0fe11b88e6f62
ftabor <ftaborconsulting@gmail.com>
2015-08-26 18:38:35 +0100

    bug with Invite button

--- public/js/ajax/userIndex.js
+++ public/js/ajax/userIndex.js
@@ -1,9 +1,10 @@
 $(function (){
 	getUsers();
-})
+	inviteClick();
+}) 
 
 function getUsers (){
-	console.log("Getting users...")
+	console.log("Getting users...");
 	$.ajax({
 		type: "get",
 		url: "/api/users",
@@ -14,16 +15,35 @@ function getUsers (){
 		//Loop through the users and create an HTML string
 		var html='',i=0;
 		for(i;i< data.users.length;i++) {
-			html += "<div class='row'><div class='large-12 columns'><div class='row'><div class='large-12 columns'><div class='large-8 columns'><div class='row'><div class='large-4 small-6 columns'><img src='"+ data.users[i].linkedin.avatar+ "'><div class='alert-box'><strong>Invite</strong>'"+ data.users[i].local.name+ "'<a href='#' class='close'>+</a></div></div></div></div></div></div></div></div>"
-		};console.log(html)
+			html += "<div class='row'><div class='large-12 columns'><div class='row'><img src='"+ data.users[i].linkedin.avatar+ "'><button href='#' class='close inviteButton'><div class='alert-box'><input type='hidden' value='"+ data.users[i]._id+"' class='invitee-id'><strong>Invite</strong>'"+ data.users[i].local.name+ "'+</button></div></div></div></div>"
+		}
+
 		$('#usersInvite').append(html)
 
 	});
-};
-
-// // Setup INVITE buttons
-// function inviteClick()
-// $('.actions .invite').click(function(e){
-// 	e.preventDefault();
-// 	postInvite()
+}
+function postInvite (userid) {
+	$.post("/api/invite", { "userId" : userid }).done(function (data) {
+		console.log(data);
+	});
+	// $.ajax({
+	// 		type: "POST",
+	// 		url: "/api/invites/",
+// 			data: { "userId": userid },
+// 			success: function 
+// 			contentType: "json",
+// 			dataType: "json"
+// 		}).done(function(data, response){
+// 			console.log(response, data)
 // });
+	}
+// Setup INVITE buttons
+function inviteClick() {
+	$('.inviteButton').on('click', function(e){
+		console.log('clicked')
+		e.preventDefault();
+		var inviteeId = $(this).children('.invitee-id').val()
+		postInvite(inviteeId);
+		$(this).removeClass('alert-box');
+	});
+}
