f04f04153a9caf3c6089ac1c5f7af1d6b0e8dd40
AnvarTurobov <anvar.turobov@gmail.com>
2015-08-26 08:53:19 +0100

    changed the picture for the homepage and playing around with css

--- public/js/ajax/show_event.js
+++ public/js/ajax/show_event.js
@@ -1,6 +1,8 @@
 
 $(function () {
   getEvents();
+
+
 });
 
 //   /**
@@ -14,38 +16,41 @@ $(function () {
 
 function getEvents () {
 
- $.ajax({
-   type: "get",
-   url: "/api/events/",
-   contentType: "json",
-   dataType: "json"
- }).done(function(data, response){
-  var html='',i=1;
-  for(i;i<data.length;i++) {
-    html += "<li class='pane"+ String(i)+"'><div class='img'></div><div>"+data[i].title+"</div><div class='like'></div><div class='dislike'></div></li>"
-  }
-  $('ul').append(html);
-  $("#tinderslide").jTinder({
+   $.ajax({
+     type: "get",
+     url: "/api/events/",
+     contentType: "json",
+     dataType: "json"
+   }).done(function(data, response){
+    var html='',i=1;
+    for(i;i<data.length;i++) {
+      html += "<li class='pane"+ String(i)+"'><div class='img'></div><div>"+data[i].title+"</div><div>"+data[i].location+"</div><div>"+data[i].message+"</div><div class='like'></div><div class='dislike'></div></li>"
+    }
+     $('ul').append(html);
+     $("#tinderslide").jTinder({
        // dislike callback
-       onDislike: function (item) {
+         onDislike: function (item) {
            // set the status text
-           $('#status').html('Dislike image ' + (item.index()+1));
+             $('#status').html('Dislike image ' + (item.index()+1));
          },
        // like callback
-       onLike: function (item) {
+         onLike: function (item) {
            // set the status text
-           $('#status').html('Like image ' + (item.index()+1));
+             $('#status').html('Like image ' + (item.index()+1));
          },  
-         animationRevertSpeed: 200,
-         animationSpeed: 400,
-         threshold: 1,
-         likeSelector: '.like',
-         dislikeSelector: '.dislike'
-       });
-  $('.actions .like, .actions .dislike').click(function(e){
-   e.preventDefault();
-   $("#tinderslide").jTinder($(this).attr('class'));
- });  
-});
+       animationRevertSpeed: 200,
+       animationSpeed: 400,
+       threshold: 1,
+       likeSelector: '.like',
+       dislikeSelector: '.dislike'
+     });
+     $('.actions .like, .actions .dislike').click(function(e){
+       e.preventDefault();
+       $("#tinderslide").jTinder($(this).attr('class'));
+     });
+
+
+    
+   });
 }
 
