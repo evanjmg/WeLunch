f79dd4dcf14103ddad95611224e6bdf384271e41
AnvarTurobov <anvar.turobov@gmail.com>
2015-08-27 15:36:42 +0100

    delete feature fixed

--- controllers/invites.js
+++ controllers/invites.js
@@ -29,29 +29,21 @@ function invitesIndex(req, res) {
 }
 
 function invitesPending(req, res) {
-  Event.find({})
+  Event.find( 
+    { "invites": { "$elemMatch": { "_invitee": req.user.id, "accepted": null  } } }
+  )
     .populate('_owner')
     .populate('invites._invitees')
-    .exec( function (err, events) {
-      if (err) res.status(403).send({ message: "Unable to retrieve invites."})
+      .exec( function (err, events) {
+        if (err) res.status(403).send({ message: "Unable to retrieve invites."})
 
-      var i=0; 
-      var j=0; 
-      var myInvitations=[];
+        console.log(events);
 
-      for(i;i< events.length;i++){
-        for (j;j< events[i].invites.length; j++) {
-          if (events[i].invites[j]._invitee == req.user.id) {
-            if (events[i].invites[j].accepted == null) myInvitations.push(events[i]);
-            break;
-          }
+        if (events) { 
+          res.status(200).send({ invites: events, currentUserId: req.user._id}); 
+        } else { 
+          res.status(404).send({ message: "Could not find any invitations."});
         }
-      }
-      
-      if (myInvitations) { 
-        res.json({ invites: myInvitations, currentUserId: req.user._id}); 
-      } else { 
-        res.json({ message: "Could not find any invitations.", myInvitations: myInvitations })}
       }); 
 }
 
@@ -84,12 +76,11 @@ function invitesAccept (req, res) {
   Event.findById(req.body.eventId, function (err, event) {
     if (err) res.json({ message: "An error occurred. Please check your request"})
     
-    var i=0;
-    for (i;i < event.invites.length;i++) {
-      if (event.invites[i]._invitee == req.user.id) {
-        event.invites[i].accepted = true;
+    event.invites.forEach(function(invite) {
+      if (invite._invitee == req.user.id) {
+        invite.accepted = true;
       } 
-    }
+    });
 
     event.save( function (err) {
       res.status(200).send({ 
@@ -104,10 +95,10 @@ function invitesDelete (req,res) {
   Event.findById(req.body.eventId, function (err, event) { 
     if (err) res.json({ message: "An error occurred"});
 
-    var i = 0;
-    for (i; i < event.invites.length; i++) {
-      if (event.invites[i]._invitee == req.user.id) event.invites[i].remove();
-    }
+    event.invites.forEach(function(invite) {
+      if (invite._invitee == req.user.id) 
+        invite.remove();
+    });
 
     event.save( function (err) {
       if (err) res.json({ message: "An error occurred"});
